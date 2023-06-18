947383a550d844ad8c72495c018b4ece790e8f6a
Grant Christensen <59584917+grantpc@users.noreply.github.com>
2023-06-18 14:32:25 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,80 @@
+import { useEffect, useReducer } from 'react';
+
+import Header from './Header';
+import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+import Question from './Question';
+
+const initialState = {
+  questions: [],
+  status: 'loading', // 'loading', 'error', 'ready', 'active', 'finished'
+  index: 0,
+  answer: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    case 'start':
+      return { ...state, status: 'active' };
+    case 'newAnswer':
+      return { ...state, answer: action.payload };
+
+    default:
+      throw new Error('Action unkown');
+  }
+}
+
+export default function App() {
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch('http://localhost:8000/questions');
+        const data = await res.json();
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (error) {
+        console.log(error);
+        dispatch({ type: 'dataError' });
+      }
+    }
+
+    fetchQuestions();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
